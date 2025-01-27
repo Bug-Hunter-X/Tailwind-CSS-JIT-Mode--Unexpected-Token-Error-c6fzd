f16075edf7d6c8d0256161c988c25ed7e91430ec
BugHunter-X <askzenai@gmail.com>
2025-01-27 18:56:24 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+// MyComponent.js
+<div className="bg-gray-100 p-4">
+  <h2 className="text-xl font-bold mb-2">Error: Unexpected token</h2>
+  <p>This error occurs because Tailwind's JIT mode isn't configured correctly or the config file is missing.</p>
+</div>
+```
